c0f0c3c8fbbd343e791bcdd501832a9f73d325fa
Stanislav <31693185+Azazelloo@users.noreply.github.com>
2020-10-08 13:01:57 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <QCoreApplication>
+#include <iostream>
+
+template < typename T >
+void InsertionSort(std::vector<T>& array){
+
+    int j;
+    for (size_t i = 1; i < array.size();i++) {
+        int key = array[i];
+        j = i - 1;
+        while (j>=0 && array[j]>key) {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = key;
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+﻿#include "Header.h"
+
+
+int main()
+{
+    std::vector<int> v={5,2,4,6,1,3};
+    InsertionSort(v);
+    return 0;
+}
