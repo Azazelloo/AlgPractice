43276fcf8a032ee0dbefffef2ea9d2ea52226a05
Stanislav <31693185+Azazelloo@users.noreply.github.com>
2020-10-09 16:02:18 +0300

    Add files via upload

--- Header.h
+++ Header.h
@@ -2,7 +2,10 @@
 
 #include <QCoreApplication>
 #include <iostream>
+#include <random>
+#include <algorithm>
 
+//сортировка вставкой
 template < typename T >
 void InsertionSort(std::vector<T>& array){
 
@@ -15,5 +18,25 @@ void InsertionSort(std::vector<T>& array){
             j--;
         }
         array[j + 1] = key;
+    }
+}
+
+
+//сортировка выбором
+template <typename T>
+void SelectionSort(std::vector<T>& array){
+
+    for(size_t i=0;i<(array.size()-1);i++){
+        size_t minIndex=array[i];
+
+        for(size_t j=i+1;j<array.size();j++){
+            if(array[j]<array[minIndex]){
+                minIndex=j;
+            }
+
+        }
+        if(minIndex!=i){
+            std::swap(array[i],array[minIndex]); //ломается здесь!
+        }
     }
 }
--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@
 
 int main()
 {
-    std::vector<int> v={5,2,4,6,1,3};
-    InsertionSort(v);
+    //делаем рандомный вектор
+    std::vector<int> v(100);
+    std::random_device r;
+    std::uniform_int_distribution<int> dist(-100,100);
+    generate(v.begin(), v.end(), [&]{return dist(r);});
+
+    //сортировка вставкой
+    //InsertionSort(v);
+
+    std::vector<int> vv={5,2,4,6,1,3};
+    //сортировка выбором
+    SelectionSort(vv); //ломается heap пофиксить
+
     return 0;
 }
